64922e17412fd21f78af4658b144a4273b8e9f07
ALLEN JOSEPH G <unknownhackerworld@gmail.com>
2025-09-03 22:41:58 +0530

    Updated Loading Page

--- src/pages/Webpage/Webpage.jsx
+++ src/pages/Webpage/Webpage.jsx
@@ -1,17 +1,32 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 import Header from '../../components/header/Header';
-import Banner from '../Webpage/Banner'
-import OurEvents from '../Webpage/OurEvents'
+import Banner from '../Webpage/Banner';
+import OurEvents from '../Webpage/OurEvents';
 import Members from '../Webpage/Members';
-import Gallery from '../Webpage/Gallery'
-import Faculties from '../Webpage/Faculties'
-import FacultyAdvisor from '../Webpage/FacultyAdvisor'
-import FacultySponsor from '../Webpage/FacultySponsor'
+import Gallery from '../Webpage/Gallery';
+import Faculties from '../Webpage/Faculties';
+import FacultyAdvisor from '../Webpage/FacultyAdvisor';
+import FacultySponsor from '../Webpage/FacultySponsor';
 import CurrentEvents from './CurrentEvents';
-import Footer from './Footer'
+import Footer from './Footer';
 import SectionDivided from '../../components/SectionDivided';
+import Logo from "../../assets/logo.png";
 
 const Webpage = () => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const handlePageLoad = () => {
+      setTimeout(() => setLoading(false), 1000); // delay for wipe animation
+    };
+
+    if (document.readyState === "complete") {
+      handlePageLoad();
+    } else {
+      window.addEventListener("load", handlePageLoad);
+      return () => window.removeEventListener("load", handlePageLoad);
+    }
+  }, []);
 
   const scrollToSection = (sectionId) => {
     const section = document.getElementById(sectionId);
@@ -25,23 +40,33 @@ const Webpage = () => {
     }
   };
 
-
   return (
-    <div className='w-full'>
+    <div className="w-full relative">
+      {/* Loader Screen */}
+      <div
+        className={`fixed inset-0 flex items-center justify-center bg-[#15ade5] z-[9999] transform transition-transform duration-1000 ${loading ? "translate-y-0" : "-translate-y-full"
+          }`}
+      >
+        <img src={Logo} alt="Logo" className="w-32 md:w-48 animate-pulse" />
+      </div>
+
+      {/* Main Content */}
+      <div>
         <Header scrollToSection={scrollToSection} />
         <Banner />
         <OurEvents />
         <Members />
-         {/* <Gallery /> */}
+        {/* <Gallery /> */}
         <SectionDivided />
         <Faculties />
-       {/* <FacultyAdvisor />
+        {/* <FacultyAdvisor />
         <FacultySponsor /> */}
         <SectionDivided />
         <CurrentEvents />
         <Footer />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Webpage
+export default Webpage;
